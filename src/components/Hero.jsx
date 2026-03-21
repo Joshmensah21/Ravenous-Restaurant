171c2e1cbc897dce8dd33d6953758f324bc5e168
J Muse <joshua.k.mensah@outlook.com>
2026-03-21 20:16:07 +0000

    feat: implemented CTA button visual change when pressed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,32 +1,33 @@
 import styles from "../css modules/Hero.module.css"
 import { useState } from "react";
 
-function FilterButton ({ label, isActive, onClick }){
-    return(
-            <li className= {`${styles.filterBtn} ${isActive? styles.active : ""}`} onClick={onClick}>
-                <h3 className = {styles.filterTitle}>{label}</h3>
-            </li>
-        
-    )
-}
-
+//PARENT COMPONENT (Cooking Pot)
 export function HeroSection (){
-    const [activeFilter, updateFilterState ]= useState("bestMatch"); 
+    const [activeFilter, updateFilterState ] = useState("bestMatch"); 
+    const [normalCTA, updateCTA] = useState("not pressed");
+    const handleClick = () => {updateCTA("pressed"); setTimeout(() => updateCTA("not pressed"), 250)}; 
+    {console.log ( "BEFORE: " + normalCTA)}
     return(
-    <div>
+    <div className={styles.activeLayer} isActive = {normalCTA === "pressed"}> {/*trying to implement cursor: progress feature across whole page when a CTA button is clicked */}
         <section className={styles.hero}>
             <div className={styles.titleContainer}>
                 <h1 className={styles.title}>ravenous</h1>
             </div>
             <div className={styles.filterContainer}>
                 <FilterButton 
-                    label = "Best Match" isActive = {activeFilter === "bestMatch"} onClick = {() => updateFilterState("bestMatch")}
+                    label = "Best Match" 
+                    isActive = {activeFilter === "bestMatch"} 
+                    onClick = {() => updateFilterState("bestMatch")}
                 />
                 <FilterButton 
-                    label = "Highest Rated" isActive = {activeFilter === "highestRated"} onClick = {() => updateFilterState("highestRated")}
+                    label = "Highest Rated" 
+                    isActive = {activeFilter === "highestRated"} 
+                    onClick = {() => updateFilterState("highestRated")}
                 />
                 <FilterButton 
-                    label = "Most Reviewed" isActive = {activeFilter === "mostReviewed"} onClick = {() => updateFilterState("mostReviewed")}
+                    label = "Most Reviewed" 
+                    isActive = {activeFilter === "mostReviewed"} 
+                    onClick = {() => updateFilterState("mostReviewed")}
                 />
             </div>
             {/*<hr className = {styles.hr}></hr>*/}
@@ -34,8 +35,34 @@ export function HeroSection (){
                 <input className={styles.searchBtn} placeholder="Search businesses"></input>
                 <input className={styles.searchBtn} placeholder="Where?"></input>
             </div>
-            <button className = {styles.letsGoBtn}><p className={styles.letsGoText}>Let's Go</p></button>
+            <CTAButton 
+                isClicked = {normalCTA === "pressed"} 
+                onClick = {handleClick}
+
+            />
+            {console.log ( "AFTER: " + normalCTA)}
+            
         </section>
     </div>
+    
 )
+
+}
+
+//CHILD COMPONENTS (ingredients)
+
+function FilterButton ({ label, isActive, onClick }){
+    return(
+            <li className = {`${styles.filterBtn} ${isActive? styles.filterActive : ""}`} onClick={onClick}>
+                <h3 className = {styles.filterTitle}>{label}</h3>
+            </li>
+        
+    )
+}
+
+function CTAButton ({isClicked, onClick}){
+    
+    return(
+            <button className = {`${styles.letsGoBtn} ${isClicked?  styles.letsGoBtnActive : ""}`} onClick={onClick}>Let's Go</button>
+    )
 }
